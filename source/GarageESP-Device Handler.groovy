5ed799bf05281921b7d8223d0900d728405a96bb
coolboarder <coolboarder654@gmail.com>
2018-12-31 08:06:52 -0800

    New Files

---
+++ source/GarageESP-Device Handler.groovy
@@ -0,0 +1,507 @@
+/**
+ *  Copyright 2018 Tom Miller
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Sonoff (Connect)
+ *
+ *  Author: Tom Miller
+ *  Date: 2018-04-20
+ */
+ 
+import groovy.json.JsonSlurper
+import groovy.util.XmlSlurper
+
+metadata {
+	definition (name: "Garage Door Controller", namespace: "GarageESP", author: "Tom Miller") {
+		capability "Actuator"
+		capability "Door Control"
+        capability "Garage Door Control"
+		capability "Contact Sensor"
+		capability "Refresh"
+		capability "Sensor"
+        capability "Lock"
+        capability "Configuration"
+        capability "Health Check"
+        
+        command "light"
+        
+        attribute   "needUpdate", "string"
+	}
+
+	simulator {
+	}
+    
+    preferences {
+        input description: "Once you change values on this page, the corner of the \"configuration\" icon will change orange until all configuration parameters are updated.", title: "Settings", displayDuringSetup: false, type: "paragraph", element: "paragraph"
+		generate_preferences(configuration_model())
+	}
+	
+	tiles {
+		standardTile("toggle", "device.door", width: 2, height: 2) {
+			state("closed", label:'${name}', action:"door control.open", icon:"st.doors.garage.garage-closed", backgroundColor:"#00A0DC", nextState:"opening")
+			state("open", label:'${name}', action:"door control.close", icon:"st.doors.garage.garage-open", backgroundColor:"#e86d13", nextState:"closing")
+			state("opening", label:'${name}', icon:"st.doors.garage.garage-opening", backgroundColor:"#e86d13")
+			state("closing", label:'${name}', icon:"st.doors.garage.garage-closing", backgroundColor:"#00A0DC")
+			
+		}
+		standardTile("open", "device.door", inactiveLabel: false, decoration: "flat") {
+			state "default", label:'open', action:"door control.open", icon:"st.doors.garage.garage-opening"
+		}
+		standardTile("close", "device.door", inactiveLabel: false, decoration: "flat") {
+			state "default", label:'close', action:"door control.close", icon:"st.doors.garage.garage-closing"
+		}
+        standardTile("light", "device.light", inactiveLabel: false, decoration: "flat") {
+            state "default", label:"Light", action:"light", icon:"st.Lighting.light11"
+        }
+		main "toggle"
+		details(["toggle", "open", "close","light"])
+	}
+
+}
+
+def unlock() {
+	def latest = device.currentState("door")
+    if(latest != null && latest.value == "closed"){
+		sendEvent(name: "door", value: "opening")
+    }
+	log.debug "on Unlock()"
+    def cmds = []
+    cmds << getAction("/open")
+//	runIn(6, finishOpening)
+    return cmds
+}
+
+def lock() {
+	def latest = device.currentState("door")
+    if(latest != null && latest.value == "open"){
+		sendEvent(name: "door", value: "closing")
+    }
+	log.debug "on Lock()"
+    def cmds = []
+    cmds << getAction("/close")
+//	runIn(6, finishClosing)
+    return cmds
+}
+
+def open() {
+	log.debug "Open Was Called!!"
+	def latest = device.currentState("door")
+    if(latest != null && latest.value == "closed"){
+		sendEvent(name: "door", value: "opening")
+    }
+	log.debug "on OPEN()"
+    def cmds = []
+    cmds << getAction("/open")
+//	runIn(6, finishOpening)
+    return cmds
+}
+
+def close() {
+	def latest = device.currentState("door")
+    if(latest != null && latest.value == "open"){
+		sendEvent(name: "door", value: "closing")
+    }
+	log.debug "on CLOSE()"
+    def cmds = []
+    cmds << getAction("/close")
+//	runIn(6, finishClosing)
+    return cmds
+}
+
+def finishOpening() {
+    // sendEvent(name: "door", value: "open")
+    // sendEvent(name: "contact", value: "open")
+}
+
+def finishClosing() {
+    // sendEvent(name: "door", value: "closed")
+    // sendEvent(name: "contact", value: "closed")
+}
+
+def installed() {
+	log.debug "installed()"
+	configure()
+}
+
+def configure() {
+    logging("configure()", 1)
+    def cmds = []
+    cmds = update_needed_settings()
+    if (cmds != []) cmds
+}
+
+def updated()
+{
+    logging("updated()", 1)
+    def cmds = [] 
+    cmds = update_needed_settings()
+    sendEvent(name: "checkInterval", value: 2 * 15 * 60 + 2 * 60, displayed: false, data: [protocol: "lan", hubHardwareId: device.hub.hardwareID])
+    sendEvent(name:"needUpdate", value: device.currentValue("needUpdate"), displayed:false, isStateChange: true)
+    if (cmds != []) response(cmds)
+}
+
+private def logging(message, level) {
+    if (logLevel != "0"){
+    switch (logLevel) {
+       case "1":
+          if (level > 1)
+             log.debug "$message"
+       break
+       case "99":
+          log.debug "$message"
+       break
+    }
+    }
+}
+
+def parse(description) {
+	//log.debug "Parsing: ${description}"
+    def events = []
+    def descMap = parseDescriptionAsMap(description)
+    def body
+    //log.debug "descMap: ${descMap}"
+
+    if (!state.mac || state.mac != descMap["mac"]) {
+		log.debug "Mac address of device found ${descMap["mac"]}"
+        updateDataValue("mac", descMap["mac"])
+	}
+    
+    if (state.mac != null && state.dni != state.mac) state.dni = setDeviceNetworkId(state.mac)
+    if (descMap["body"]) body = new String(descMap["body"].decodeBase64())
+
+    if (body && body != "") {
+    
+        if(body.startsWith("{") || body.startsWith("[")) {
+            def slurper = new JsonSlurper()
+            def result = slurper.parseText(body)
+
+            log.debug "aresult!: ${result}"
+
+            if (result.containsKey("type")) {
+                if (result.type == "configuration")
+                    events << update_current_properties(result)
+            }
+            
+            if (result.containsKey("type")) {
+                if (result.type == "configuration")
+                    events << update_current_properties(result)
+                if (result.type == "relay")
+                    parseRelay(result)
+                 if (result.type == "door"){
+                 	sendEvent(name: "door", value: result.state)
+                    sendEvent(name: "contact", value: result.state)
+                    events << createEvent(name: "lock", value: result.state == "closed" ? "locked" : "unlocked")
+                 }
+ //                if (result.type == "contact"){
+//                 	sendEvent(name: "door", value: result.state)
+//                    sendEvent(name: "contact", value: result.state)
+//                 }
+            }
+
+            if (result.containsKey("uptime")) {
+                events << createEvent(name: "uptime", value: result.uptime, displayed: false)
+            }
+        } else {
+            //log.debug "Response is not JSON: ${body}"
+        }
+    }
+
+    if (!device.currentValue("ip") || (device.currentValue("ip") != getDataValue("ip"))) events << createEvent(name: 'ip', value: getDataValue("ip"))
+
+    return events
+}
+
+def parseRelay(cmd) {
+    if (cmd.number != "0") {
+    } else {
+        sendEvent(name: "switch", value: cmd.power)
+    }
+}
+
+def parseDescriptionAsMap(description) {
+	description.split(",").inject([:]) { map, param ->
+		def nameAndValue = param.split(":")
+        
+        if (nameAndValue.length == 2) map += [(nameAndValue[0].trim()):nameAndValue[1].trim()]
+        else map += [(nameAndValue[0].trim()):""]
+	}
+}
+
+def refresh() {
+	log.debug "refresh()"
+    def cmds = []
+    cmds << getAction("/status")
+    return cmds
+}
+
+def ping() {
+    log.debug "ping()"
+    refresh()
+}
+
+private getAction(uri){ 
+  updateDNI()
+  def userpass
+  //log.debug uri
+  if(password != null && password != "") 
+    userpass = encodeCredentials("admin", password)
+    
+  def headers = getHeader(userpass)
+
+  def hubAction = new physicalgraph.device.HubAction(
+    method: "GET",
+    path: uri,
+    headers: headers
+  )
+  return hubAction    
+}
+
+private postAction(uri, data){ 
+  updateDNI()
+  
+  def userpass
+  
+  if(password != null && password != "") 
+    userpass = encodeCredentials("admin", password)
+  
+  def headers = getHeader(userpass)
+  
+  def hubAction = new physicalgraph.device.HubAction(
+    method: "POST",
+    path: uri,
+    headers: headers,
+    body: data
+  )
+  return hubAction    
+}
+
+private setDeviceNetworkId(ip, port = null){
+    def myDNI
+    if (port == null) {
+        myDNI = ip
+    } else {
+  	    def iphex = convertIPtoHex(ip)
+  	    def porthex = convertPortToHex(port)
+        myDNI = "$iphex:$porthex"
+    }
+    log.debug "Device Network Id set to ${myDNI}"
+    return myDNI
+}
+
+private updateDNI() { 
+    if (state.dni != null && state.dni != "" && device.deviceNetworkId != state.dni) {
+       device.deviceNetworkId = state.dni
+    }
+}
+
+private getHostAddress() {
+    if (override == "true" && ip != null && ip != ""){
+        return "${ip}:80"
+    }
+    else if(getDeviceDataByName("ip") && getDeviceDataByName("port")){
+        return "${getDeviceDataByName("ip")}:${getDeviceDataByName("port")}"
+    }else{
+	    return "${ip}:80"
+    }
+}
+
+private String convertIPtoHex(ipAddress) { 
+    String hex = ipAddress.tokenize( '.' ).collect {  String.format( '%02x', it.toInteger() ) }.join()
+    return hex
+}
+
+private String convertPortToHex(port) {
+	String hexport = port.toString().format( '%04x', port.toInteger() )
+    return hexport
+}
+
+private encodeCredentials(username, password){
+	def userpassascii = "${username}:${password}"
+    def userpass = "Basic " + userpassascii.encodeAsBase64().toString()
+    return userpass
+}
+
+private getHeader(userpass = null){
+    def headers = [:]
+    headers.put("Host", getHostAddress())
+    headers.put("Content-Type", "application/x-www-form-urlencoded")
+    if (userpass != null)
+       headers.put("Authorization", userpass)
+    return headers
+}
+
+def light() {
+	log.debug "light()"
+    def uri = "/light"
+    getAction(uri)
+}
+
+def sync(ip, port) {
+    def existingIp = getDataValue("ip")
+    def existingPort = getDataValue("port")
+    if (ip && ip != existingIp) {
+        updateDataValue("ip", ip)
+        sendEvent(name: 'ip', value: ip)
+    }
+    if (port && port != existingPort) {
+        updateDataValue("port", port)
+    }
+}
+
+def generate_preferences(configuration_model)
+{
+    def configuration = parseXml(configuration_model)
+   
+    configuration.Value.each
+    {
+        if(it.@hidden != "true" && it.@disabled != "true"){
+        switch(it.@type)
+        {   
+            case ["number"]:
+                input "${it.@index}", "number",
+                    title:"${it.@label}\n" + "${it.Help}",
+                    range: "${it.@min}..${it.@max}",
+                    defaultValue: "${it.@value}",
+                    displayDuringSetup: "${it.@displayDuringSetup}"
+            break
+            case "list":
+                def items = []
+                it.Item.each { items << ["${it.@value}":"${it.@label}"] }
+                input "${it.@index}", "enum",
+                    title:"${it.@label}\n" + "${it.Help}",
+                    defaultValue: "${it.@value}",
+                    displayDuringSetup: "${it.@displayDuringSetup}",
+                    options: items
+            break
+            case ["password"]:
+                input "${it.@index}", "password",
+                    title:"${it.@label}\n" + "${it.Help}",
+                    displayDuringSetup: "${it.@displayDuringSetup}"
+            break
+            case "decimal":
+               input "${it.@index}", "decimal",
+                    title:"${it.@label}\n" + "${it.Help}",
+                    range: "${it.@min}..${it.@max}",
+                    defaultValue: "${it.@value}",
+                    displayDuringSetup: "${it.@displayDuringSetup}"
+            break
+            case "boolean":
+               input "${it.@index}", "boolean",
+                    title:"${it.@label}\n" + "${it.Help}",
+                    defaultValue: "${it.@value}",
+                    displayDuringSetup: "${it.@displayDuringSetup}"
+            break
+        }
+        }
+    }
+}
+
+ /*  Code has elements from other community source @CyrilPeponnet (Z-Wave Parameter Sync). */
+
+def update_current_properties(cmd)
+{
+    def currentProperties = state.currentProperties ?: [:]
+    currentProperties."${cmd.name}" = cmd.value
+
+    if (settings."${cmd.name}" != null)
+    {
+        if (settings."${cmd.name}".toString() == cmd.value)
+        {
+            sendEvent(name:"needUpdate", value:"NO", displayed:false, isStateChange: true)
+        }
+        else
+        {
+            sendEvent(name:"needUpdate", value:"YES", displayed:false, isStateChange: true)
+        }
+    }
+    state.currentProperties = currentProperties
+}
+
+
+def update_needed_settings()
+{
+    def cmds = []
+    def currentProperties = state.currentProperties ?: [:]
+     
+    def configuration = parseXml(configuration_model())
+    def isUpdateNeeded = "NO"
+    
+    cmds << getAction("/configSet?name=haip&value=${device.hub.getDataValue("localIP")}")
+    cmds << getAction("/configSet?name=haport&value=${device.hub.getDataValue("localSrvPortTCP")}")
+    
+    configuration.Value.each
+    {     
+        if ("${it.@setting_type}" == "lan" && it.@disabled != "true"){
+            if (currentProperties."${it.@index}" == null)
+            {
+               if (it.@setonly == "true"){
+                  logging("Setting ${it.@index} will be updated to ${it.@value}", 2)
+                  cmds << getAction("/configSet?name=${it.@index}&value=${it.@value}")
+               } else {
+                  isUpdateNeeded = "YES"
+                  logging("Current value of setting ${it.@index} is unknown", 2)
+                  cmds << getAction("/configGet?name=${it.@index}")
+               }
+            }
+            else if ((settings."${it.@index}" != null || it.@hidden == "true") && currentProperties."${it.@index}" != (settings."${it.@index}"? settings."${it.@index}".toString() : "${it.@value}"))
+            { 
+                isUpdateNeeded = "YES"
+                logging("Setting ${it.@index} will be updated to ${settings."${it.@index}"}", 2)
+                cmds << getAction("/configSet?name=${it.@index}&value=${settings."${it.@index}"}")
+            } 
+        }
+    }
+    
+    sendEvent(name:"needUpdate", value: isUpdateNeeded, displayed:false, isStateChange: true)
+    return cmds
+}
+
+def configuration_model()
+{
+'''
+<configuration>
+<Value type="password" byteSize="1" index="password" label="Password" min="" max="" value="" setting_type="preference" fw="">
+<Help>
+</Help>
+</Value>
+<Value type="list" byteSize="1" index="pos" label="Boot Up State" min="0" max="2" value="0" setting_type="lan" fw="">
+<Help>
+Default: Off
+</Help>
+    <Item label="Off" value="0" />
+    <Item label="On" value="1" />
+    <Item label="Previous" value="2" />
+</Value>
+<Value type="number" byteSize="1" index="autooff" label="Auto Off" min="0" max="65536" value="0" setting_type="lan" fw="">
+<Help>
+Automatically turn the switch off after this many seconds.
+Range: 0 to 65536
+Default: 0 (Disabled)
+</Help>
+</Value>
+<Value type="list" byteSize="1" index="switchtype" label="External Switch Type" min="0" max="1" value="0" setting_type="lan" fw="">
+<Help>
+If a switch is attached to GPIO 14.
+Default: Momentary
+</Help>
+    <Item label="Momentary" value="0" />
+    <Item label="Toggle" value="1" />
+</Value>
+<Value type="list" index="logLevel" label="Debug Logging Level?" value="0" setting_type="preference" fw="">
+<Help>
+</Help>
+    <Item label="None" value="0" />
+    <Item label="Reports" value="1" />
+    <Item label="All" value="99" />
+</Value>
+</configuration>
+'''
+}
