e0fd065c686310e7cc9f66fca59830f24a244953
Tom Miller <coolboarder654@gmail.com>
2019-11-16 21:09:46 -0800

    Updated.

--- smartapps/garageesp/esp8266-garage-door-connect.src/esp8266-garage-door-connect.groovy
+++ smartapps/garageesp/esp8266-garage-door-connect.src/esp8266-garage-door-connect.groovy
@@ -10,7 +10,7 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  Sonoff (Connect)
+ *  ESP8266 Garage Door (Connect)
  *
  *  Author: Coolboarder
  *  Date: 2018-12-31
